defaa285b042ae865976025bdebb41a2d20debe8
bhavyajoshi <bjoshi1503@gmail.com>
2025-06-25 22:54:55 +0530

    backend deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://chat-app-beryl-eta.vercel.app'],
     credentials: true
 }
 ))
